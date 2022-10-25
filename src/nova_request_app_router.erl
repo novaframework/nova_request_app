50693eced4cdacab11be6ca298fee79819b27476
Daniel Widgren <daniel.widgren@gmail.com>
2022-10-26 00:21:55 +0200

    added tests for 405

--- src/nova_request_app_router.erl
+++ src/nova_request_app_router.erl
@@ -20,7 +20,6 @@ routes(_) ->
       #{prefix => "/json_binding",
         security => false,
         routes => [{"/", {nova_request_app_main_controller, all}, #{methods => [get]}},
-                   {"/:json", { nova_request_app_main_controller, json_get_binding}, #{methods => [get]}},
                    {"/:json/", { nova_request_app_main_controller, json_get_binding}, #{methods => [get]}}
                   ]
         },
