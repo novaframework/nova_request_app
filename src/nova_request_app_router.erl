065b7d523efa965b372b89c49682da7bf30e16ff
Daniel Widgren <daniel.widgren@gmail.com>
2024-05-24 22:42:18 +0200

    added fun routing and fun tests

--- src/nova_request_app_router.erl
+++ src/nova_request_app_router.erl
@@ -4,7 +4,10 @@
 -export([routes/1]).
 
 routes(_) ->
-    [#{prefix => "",
+    no_fun() ++ with_fun().
+
+no_fun() ->
+  [#{prefix => "",
        security => false,
        routes => [
                   {"/", { nova_request_app_main_controller, json_get}, #{methods => [get]}},
@@ -40,5 +43,41 @@ routes(_) ->
       ]},
           #{prefix => "",
         security => false,
-        routes => [{"/trailingslash/", {nova_request_app_main_controller, json_get}, #{methods => [get]}}]}
-    ].
+        routes => [{"/trailingslash/", {nova_request_app_main_controller, json_get}, #{methods => [get]}}]}].
+
+with_fun() ->
+  [#{prefix => "/fun/",
+        security => false,
+        routes => [
+                  {"/",  fun nova_request_app_main_controller:json_get/1, #{methods => [get]}},
+                  {"/get_qs",  fun nova_request_app_main_controller:get_qs/1, #{methods => [get]}},
+                  {"/post_params", fun nova_request_app_main_controller:post_params/1, #{methods => [post]}},
+                  {"/json_post", fun nova_request_app_main_controller:json_post/1, #{methods => [post]}},
+                  {"/json_get", fun nova_request_app_main_controller:json_get/1, #{methods => [get]}},
+                  {"/ws/:ws", nova_request_ws, #{protocol => ws}},
+                  {"/session/:session", fun nova_request_app_main_controller:session/1, #{methods => [get]}},
+                  {"/internalerror", fun nova_request_app_main_controller:internal_server_error/1 , #{methods => [get]}},
+                  {"/fallback", fun nova_request_app_main_controller:fallback/1, #{methods => [get]}},
+                  {"/viewok", fun nova_request_app_main_controller:return_view_with_ok/1, #{methods => [get]}},
+                  {"/viewview", fun nova_request_app_main_controller:return_view_with_view/1 , #{methods => [get]}},
+                  {"/trailingslash/", fun nova_request_app_main_controller:json_get/1, #{methods => [get]}}
+                  ]
+      },
+      #{prefix => "/fun/json_binding",
+        security => false,
+        routes => [{"/", fun nova_request_app_main_controller:all/1, #{methods => [get]}},
+                    {"/:json/", fun nova_request_app_main_controller:json_get_binding/1, #{methods => [get]}}
+                  ]
+        },
+      #{prefix => "/fun/secure",
+        security => fun nova_request_auth:auth/1,
+        routes => [{"/:secure", fun nova_request_app_main_controller:secure/1 , #{methods => [get]}},
+                    {"/apanws/:ws", nova_request_ws, #{protocol => ws}}]
+      },
+      #{prefix => "/fun/user/:userid",
+        security => false,
+        routes => [
+                    {"/", fun nova_request_app_main_controller:get_user/1, #{methods => [get]}},
+                    {"/", fun nova_request_app_main_controller:delete_user/1, #{methods => [delete]}}
+
+      ]}].
---
+++ test/nova_request_app_fun_SUITE.erl
@@ -0,0 +1,297 @@
+-module(nova_request_app_fun_SUITE).
+
+-compile(export_all).
+
+-include_lib("common_test/include/ct.hrl").
+
+-define(BASEPATH, <<"http://localhost:8080/fun/">>).
+
+%%--------------------------------------------------------------------
+%% @spec suite() -> Info
+%% Info = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+suite() ->
+    [{timetrap,{seconds,30}}].
+
+%%--------------------------------------------------------------------
+%% @spec init_per_suite(Config0) ->
+%%     Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_suite(_Config) ->
+    application:ensure_all_started(ssl),
+    application:ensure_all_started(gun),
+    application:ensure_all_started(nova_request_app),
+    [].
+
+%%--------------------------------------------------------------------
+%% @spec end_per_suite(Config0) -> term() | {save_config,Config1}
+%% Config0 = Config1 = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+end_per_suite(Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec init_per_group(GroupName, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% GroupName = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_group(_GroupName, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @spec end_per_group(GroupName, Config0) ->
+%%               term() | {save_config,Config1}
+%% GroupName = atom()
+%% Config0 = Config1 = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+end_per_group(_GroupName, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec init_per_testcase(TestCase, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% TestCase = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @spec end_per_testcase(TestCase, Config0) ->
+%%               term() | {save_config,Config1} | {fail,Reason}
+%% TestCase = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec groups() -> [Group]
+%% Group = {GroupName,Properties,GroupsAndTestCases}
+%% GroupName = atom()
+%% Properties = [parallel | sequence | Shuffle | {RepeatType,N}]
+%% GroupsAndTestCases = [Group | {group,GroupName} | TestCase]
+%% TestCase = atom()
+%% Shuffle = shuffle | {shuffle,{integer(),integer(),integer()}}
+%% RepeatType = repeat | repeat_until_all_ok | repeat_until_all_fail |
+%%              repeat_until_any_ok | repeat_until_any_fail
+%% N = integer() | forever
+%% @end
+%%--------------------------------------------------------------------
+groups() ->
+    [].
+
+%%--------------------------------------------------------------------
+%% @spec all() -> GroupsAndTestCases | {skip,Reason}
+%% GroupsAndTestCases = [{group,GroupName} | TestCase]
+%% GroupName = atom()
+%% TestCase = atom()
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+all() ->
+    [
+     get_qs,
+     post_params,
+     post_json,
+     get_json,
+     get_json_root,
+     get_json_binding,
+     get_json_content_type,
+     get_all,
+     ws,
+     ws_secure,
+     get_secure,
+     not_found,
+     internal_server_error,
+     get_user,
+     get_user2,
+     delete_user,
+     delete_user2,
+     trailingslash,
+     fallback,
+     view_with_ok,
+     view_with_view].
+%%--------------------------------------------------------------------
+%% @spec TestCase(Config0) ->
+%%               ok | exit() | {skip,Reason} | {comment,Comment} |
+%%               {save_config,Config1} | {skip_and_save,Reason,Config1}
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% Comment = term()
+%% @end
+%%--------------------------------------------------------------------
+
+get_qs(_) ->
+    Path = [?BASEPATH, <<"get_qs?ordered_by=name">>],
+    #{status := {200, _}, body := RespBody} = shttpc:get(Path, opts()),
+    #{<<"ordered_by">> := <<"name">>} = json:decode(RespBody, [maps]).
+
+post_params(_) ->
+    Path = [?BASEPATH, <<"post_params">>],
+    Params = <<"field1=value1&field2=value2">>,
+    #{status := {201, _}, body := RespBody} = shttpc:post(Path, Params, opts(form)),
+    #{<<"field1">> := <<"value1">>,
+      <<"field2">> := <<"value2">>} = json:decode(RespBody, [maps]).
+
+post_json(_) ->
+    Path = [?BASEPATH, <<"json_post">>],
+    Json = #{<<"field1">> => <<"value1">>,
+             <<"field2">> => <<"value2">>},
+    #{status := {201, _}, body := RespBody} = shttpc:post(Path, encode(Json), opts(json_post)),
+    #{<<"field1">> := <<"value1">>,
+      <<"field2">> := <<"value2">>} = json:decode(RespBody, [maps]).
+
+get_json(_) ->
+    Path = [?BASEPATH, <<"json_get">>],
+    #{status := {200, _}, body := RespBody} = shttpc:get(Path, opts(json_get)),
+    #{<<"test">> := <<"json">>} = json:decode(RespBody, [maps]).
+
+get_json_root(_) ->
+    Path = [?BASEPATH],
+    #{status := {200, _}, body := RespBody} = shttpc:get(Path, opts(json_get)),
+    #{<<"test">> := <<"json">>} = json:decode(RespBody, [maps]).
+
+get_json_binding(_) ->
+    Path = [?BASEPATH, <<"json_binding/apan">>],
+    #{status := {200, _}, body := RespBody} = shttpc:get(Path, opts(json_get)),
+    #{<<"test">> := <<"apan">>} = json:decode(RespBody, [maps]).
+
+get_json_content_type(_) ->
+    Path = [?BASEPATH, <<"json_get">>],
+    #{status := {200, _}, body := RespBody} = shttpc:get(Path, opts(json_post)),
+    #{<<"test">> := <<"json">>} = json:decode(RespBody, [maps]).
+get_all(_) ->
+    Path = [?BASEPATH, <<"json_binding">>],
+    #{status := {200, _}, body := RespBody} = shttpc:get(Path, opts(json_get)),
+    CorrectAnswer = [#{<<"id">> => X} || X <- lists:seq(1, 10)],
+    CorrectAnswer = json:decode(RespBody, [maps]).
+
+get_secure(_) ->
+    Path = [?BASEPATH, <<"secure/apan">>],
+    #{status := {200, _}, body := RespBody} = shttpc:get(Path, opts(json_get)),
+    #{<<"secure">> := <<"apan">>} = json:decode(RespBody, [maps]).
+
+not_found(_) ->
+    Path = [?BASEPATH, <<"notfound">>],
+    #{status := {404, _}} = shttpc:get(Path, opts(json_get)).
+
+
+internal_server_error(_) ->
+    Path = [?BASEPATH, <<"internalerror">>],
+    #{status := {500, _}} = shttpc:get(Path, opts(json_get)).
+
+
+get_user(_) ->
+    UserId = <<"1">>,
+    Path = [?BASEPATH, <<"user/">>, UserId],
+    #{status := {200, _}, body := Body} = shttpc:get(Path, opts(json_get)),
+    #{<<"id">> := <<"1">>} = json:decode(Body, [maps]).
+
+get_user2(_) ->
+    UserId = <<"1">>,
+    Path = [?BASEPATH, <<"user/">>, UserId, <<"/">>],
+    #{status := {200, _}, body := Body} = shttpc:get(Path, opts(json_get)),
+    #{<<"id">> := <<"1">>} = json:decode(Body, [maps]).
+
+delete_user(_) ->
+    UserId = <<"1">>,
+    Path = [?BASEPATH, <<"user/">>, UserId],
+    #{status := {204, _}} = shttpc:delete(Path, opts(json_get)).
+
+delete_user2(_) ->
+    UserId = <<"1">>,
+    Path = [?BASEPATH, <<"user/">>, UserId, <<"/">>],
+    #{status := {204, _}} = shttpc:delete(Path, opts(json_get)).
+
+trailingslash(_) ->
+    Path = [?BASEPATH, <<"trailingslash">>],
+    #{status := {200, _}, body := RespBody} = shttpc:get(Path, opts(json_get)),
+    #{<<"test">> := <<"json">>} = json:decode(RespBody, [maps]).
+
+fallback(_) ->
+    Path = [?BASEPATH, <<"fallback">>],
+    #{status := {400, _}} = shttpc:get(Path, opts(json_get)).
+
+view_with_ok(_) ->
+    Path = [?BASEPATH, <<"viewok">>],
+    #{status := {200, _}} = shttpc:get(Path, opts(json_get)).
+
+view_with_view(_) ->
+    Path = [?BASEPATH, <<"viewview">>],
+    #{status := {200, _}} = shttpc:get(Path, opts(json_get)).
+
+ws(_) ->
+    Wohoo = <<"wohoo">>,
+    websocket([<<"/ws/">>, Wohoo], <<>>),
+    receive
+    {gun_ws, _ConnPid, _StreamRef0, {text, Response}} ->
+        io:format("~p", [Response]),
+        Wohoo = Response
+    after 8000 ->
+        exit(timeout)
+    end.
+
+ws_secure(_) ->
+    Wohoo = <<"wohoo">>,
+    websocket([<<"/secure/apanws/">>, Wohoo], <<>>),
+    receive
+    {gun_ws, _ConnPid, _StreamRef0, {text, Response}} ->
+        io:format("~p", [Response]),
+        Wohoo = Response
+    after 8000 ->
+        exit(timeout)
+    end.
+
+session(_) ->
+
+    Path = [?BASEPATH, <<"session/">>],
+    #{status := {200, _}, body := RespBody} = shttpc:get(Path, opts(json_get)),
+    #{<<"test">> := <<"json">>} = json:decode(RespBody, [maps]).
+opts() ->
+    opts(undefined).
+opts(undefined) ->
+    #{headers => #{}, close => true};
+opts(form) ->
+    #{headers => #{'Content-Type' => <<"application/x-www-form-urlencoded">>}, close => true};
+opts(json_get) ->
+    #{headers => #{'Accept' => <<"application/json">>}, close => true};
+opts(json_post) ->
+    #{headers => #{'Content-Type' => <<"application/json">>}, close => true}.
+
+websocket(Path, _Token) ->
+    {ok, ConnPid} = gun:open("localhost", 8080, #{transport => tcp}),
+    {ok, _Protocol} = gun:await_up(ConnPid),
+    io:format("ConnPid: ~p", [ConnPid]),
+    gun:ws_upgrade(ConnPid, Path, []),
+
+    receive
+        {gun_upgrade, ConnPid, _StreamRef, _Protocols, _Headers} ->
+            ConnPid;
+        {gun_response, ConnPid, _, _, Status, Headers} ->
+            exit({ws_upgrade_failed, Status, Headers});
+        {gun_error, _ConnPid, _StreamRef, Reason} ->
+            exit({ws_upgrade_failed, Reason});
+        Err ->
+            io:format("WS unexpectedly received ~p", [Err])
+
+            %% More clauses here as needed.
+    after 2000 ->
+            exit(timeout)
+    end.
+
+encode(Json) ->
+    json:encode(Json, [maps, binary]).
